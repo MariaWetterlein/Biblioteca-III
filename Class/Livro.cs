924518758e1308b7867f108d994a1ed21ef6726e
luanrodrigueseduardo <167102015+luanrodrigueseduardo@users.noreply.github.com>
2024-08-16 21:21:17 -0300

    Create Livro.cs

---
+++ Class/Livro.cs
@@ -0,0 +1,14 @@
+public class Livro()
+
+{
+	public int id{ get; set; }
+	public string Nome { get; set; }
+	public string Autor { get; set; }
+	public bool Disponivel { get; set; }
+	
+	public void ExibirInformacoes()
+	
+	{
+		
+	}
+}
